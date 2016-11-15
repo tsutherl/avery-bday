982cefec47d584afb88affbee91b75d1387abf73
Tatiana Sutherland <tsutherl@oberlin.edu>
2016-11-14 20:27:32 -0500

    tried adding #D text

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-/*
-Audio Visualizer by Raathigeshan.
-http://raathigesh.com/
-*/
-
 var visualizer;
 
 $(document).ready(function () {
@@ -16,8 +11,7 @@ $(document).ready(function () {
 
 
 function AudioVisualizer() {
-    //constants
-    this.numberOfBars = 60;
+    this.numberOfBars = 30;
 
     //Rendering
     this.scene;
@@ -27,13 +21,21 @@ function AudioVisualizer() {
 
     //bars
     this.bars = new Array();
+    this.flatBars = new Array();
 
     //audio
     this.javascriptNode;
     this.audioContext;
     this.sourceBuffer;
-    this.analyser;
+    this.analyzer;
 }
+
+// function loadFont() {
+//     var loader = new THREE.FontLoader();
+//     loader.load( 'fonts/' + fontName + '_' + fontWeight + '.typeface.json', function ( response ) {
+//     font = response;
+//     } );
+// }
 
 //initialize the visualizer elements
 AudioVisualizer.prototype.initialize = function () {
@@ -48,12 +50,12 @@ AudioVisualizer.prototype.initialize = function () {
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setSize(WIDTH, HEIGHT);
 
-    //append the rederer to the body
+    //append the renderer to the body
     document.body.appendChild(this.renderer.domElement);
 
     //create and add camera
     this.camera = new THREE.PerspectiveCamera(40, WIDTH / HEIGHT, 0.1, 20000);
-    this.camera.position.set(0, 45, 0);
+    this.camera.position.set(0, 20, 20);
     this.scene.add(this.camera);
 
     var that = this;
@@ -71,36 +73,63 @@ AudioVisualizer.prototype.initialize = function () {
 
     });
 
-    //background color of the scene
-    this.renderer.setClearColor(0x333F47, 1);
-
     //create a light and add it to the scene
     var light = new THREE.PointLight(0xffffff);
     light.position.set(-100, 200, 100);
     this.scene.add(light);
 
-    //Add interation capability to the scene
+    //add controls to the scene (zoom, rotate...)
     this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
 };
 
+
 //create the bars required to show the visualization
 AudioVisualizer.prototype.createBars = function () {
+
+
+
+    var loader = new THREE.FontLoader();
+
+    loader.load( 'fonts/helvetiker_bold.typeface.js', function ( font ) {
+        console.log('inside')
+        var material = new THREE.MeshPhongMaterial({
+            color: 'white'
+        });
+        var textGeom = new THREE.TextGeometry( 'Hello, World!', {
+            font: font
+        });
+
+        this.flatBars[0] = new THREE.Mesh( textGeom, material );
+
+        textGeom.computeBoundingBox();
+        this.flatBars[0] = textGeom.boundingBox.max.x - textGeom.boundingBox.min.x;
+        
+        textMesh.position.set( -0.5 * textWidth, 100, 0 );
+        scene.add(this.flatBars[0]  );
+
+    } )
+     
+    
+
 
     //iterate and create bars
     for (var i = 0; i < this.numberOfBars; i++) {
 
         //create a bar
-        var barGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+        var curve = new THREE.SplineCurve3([
+            new THREE.Vector3(0,0,-14),
+            new THREE.Vector3( 0, .2, 0 ),
+            new THREE.Vector3(0, 0, 10 ),
+        ] );
+
+        var geometry = new THREE.Geometry();
+        geometry.vertices = curve.getPoints( 50 );
 
         //create a material
-        var material = new THREE.MeshPhongMaterial({
-            color: this.getRandomColor(),
-            ambient: 0x808080,
-            specular: 0xffffff
-        });
+        var material = new THREE.LineBasicMaterial( { color : 'white' } );
 
         //create the geometry and set the initial position
-        this.bars[i] = new THREE.Mesh(barGeometry, material);
+        this.bars[i] = new THREE.Line( geometry, material );
         this.bars[i].position.set(i - this.numberOfBars/2, 0, 0);
 
         //add the created bar to the scene
@@ -112,36 +141,36 @@ AudioVisualizer.prototype.setupAudioProcessing = function () {
     //get the audio context
     this.audioContext = new AudioContext();
     //create the javascript node
+    //the buffer size must be a certain value and 4096 was one of the options 
     this.javascriptNode = this.audioContext.createScriptProcessor(4096, 1, 1);
     this.javascriptNode.connect(this.audioContext.destination);
 
     //create the source buffer
     this.sourceBuffer = this.audioContext.createBufferSource();
 
-    //create the analyser node
-    this.analyser = this.audioContext.createAnalyser();
-    this.analyser.smoothingTimeConstant = 0.3;
+    //create the analyzer node
+    this.analyzer = this.audioContext.createAnalyser();
+    this.analyzer.smoothingTimeConstant = 0.3;
 
-    this.analyser.fftSize = 512;
+    this.analyzer.fftSize = 512;
 
+    //connect source to the analyzer node
+    this.sourceBuffer.connect(this.analyzer);
 
-    //connect source to analyser
-    this.sourceBuffer.connect(this.analyser);
+    //analyzer to speakers
+    this.analyzer.connect(this.javascriptNode);
 
-    //analyser to speakers
-    this.analyser.connect(this.javascriptNode);
-
-    //connect source to analyser
+    //connects to the destination of the sound
     this.sourceBuffer.connect(this.audioContext.destination);
-
+    //the above lets us get info on the frequencies of the audio in real
     var that = this;
 
     //this is where we animates the bars
     this.javascriptNode.onaudioprocess = function () {
 
-        // get the average for the first channel
-        var array = new Uint8Array(that.analyser.frequencyBinCount);
-        that.analyser.getByteFrequencyData(array);
+        //store this data in a byte array
+        var array = new Uint8Array(that.analyzer.frequencyBinCount);
+        that.analyzer.getByteFrequencyData(array);
 
         //render the scene and update controls
         visualizer.renderer.render(visualizer.scene, visualizer.camera);
@@ -150,24 +179,22 @@ AudioVisualizer.prototype.setupAudioProcessing = function () {
         var step = Math.round(array.length / visualizer.numberOfBars);
 
         //Iterate through the bars and scale the z axis
+        //left = treble, middle = middle sounds/bass 
         for (var i = 0; i < visualizer.numberOfBars; i++) {
             var value = array[i * step] / 4;
             value = value < 1 ? 1 : value;
-            visualizer.bars[i].scale.z = value;
+            visualizer.bars[i].scale.y = value;
         }
     }
 
 };
 
-//get the default audio from the server
+//get audio from the server
 AudioVisualizer.prototype.getAudio = function () {
     var request = new XMLHttpRequest();
     request.open("GET", 'concatenated_file.mp3', true);
     request.responseType = "arraybuffer";
     request.send();
-    // request.onload = function () {
-    //     //that.start(request.response);
-    // }
 };
 
 //start the audio processing
@@ -185,19 +212,11 @@ AudioVisualizer.prototype.start = function (buffer) {
 
     function decodeAudioDataFailed() {
         console.log('failed')
-        // debugger
     }
 };
 
-//util method to get random colors to make stuff interesting
-AudioVisualizer.prototype.getRandomColor = function () {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-};
+// AudioVisualizer.prototype.stop = function () {
+// }
 
 AudioVisualizer.prototype.handleDrop = function () {
     //drag Enter
@@ -238,9 +257,21 @@ AudioVisualizer.prototype.handleDrop = function () {
         };
 
         fileReader.onerror = function (e) {
-          // debugger
+          console.error(e)
         };
        
         fileReader.readAsArrayBuffer(file);
     }, false);
+
+
 }
+
+
+
+
+
+
+
+
+
+
